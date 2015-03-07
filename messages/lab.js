a4ce377e4dd5b4eaa47282dece7a4ff84cad29b4
Kyle Slinger <KySlinger@sbcglobal.net>
2015-03-06 20:54:04 -0500

    added console

--- messages/lab.js
+++ messages/lab.js
@@ -4,12 +4,12 @@ function parse () {
 console.log(request.readyState);
 	request.send();
 	console.log(request.readyState);
-	if(request.readyState == 4 && request.status == 200) {
+	//if(request.readyState == 4 && request.status == 200) {
 	    	content = request.responseText;
 		console.log(request.readyState);
 		document.getElementById("messages").innerHTML = content;
 		alert(content);		
-	}
+	//}
 	
 
 }
