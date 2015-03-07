0325a0b6fa5febb664f8ffdedf3410ff996427b3
Kyle Slinger <KySlinger@sbcglobal.net>
2015-03-06 20:10:42 -0500

    debugging

--- messages/lab.js
+++ messages/lab.js
@@ -1,11 +1,11 @@
 function parse () {
 	request = new XMLHttpRequest();
 	
-	if(request.readyState == 4 && request.status == 200) {
+	//if(request.readyState == 4 && request.status == 200) {
 		content = JSON.parse(request.responseText);
 		
 		document.getElementById("messages").innerHTML = content;		
-	}
+	//}
 	request.open("GET", "data.json", true);	
 	request.send();
 
