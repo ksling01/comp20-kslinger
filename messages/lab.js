5a27acefeddc4b15fed05c83327deb1a1fb976ad
Kyle Slinger <KySlinger@sbcglobal.net>
2015-03-06 20:39:55 -0500

    added console

--- messages/lab.js
+++ messages/lab.js
@@ -1,12 +1,13 @@
 function parse () {
 	request = new XMLHttpRequest();
-	
-	//if(request.readyState == 4 && request.status == 200) {
-		content = JSON.parse(request.responseText);
-		console.log("request.readyState");
-		document.getElementById("messages").innerHTML = content;		
-	//}
 	request.open("GET", "data.json", true);	
 	request.send();
+	//if(request.readyState == 4 && request.status == 200) {
+	   var 	content = JSON.parse(request.responseText);
+		console.log(request.readyState);
+		document.getElementById("messages").innerHTML = content;
+		alert(content);		
+	//}
+	
 
 }
